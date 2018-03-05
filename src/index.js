bbeb4ec53744d6691928d04adce2b7ba2dec8f57
Lawre <camperinc@gmail.com>
2018-03-05 17:25:51 +0300

    Finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+  module.exports = function check(str, bracketsConfig) {
+    let stack = [];
+    let last_in_stack;
+    let mas = str.split('');
+    let solution = true;
+    if (mas.length % 2 != 0) return false;
+    for (let k = 0; k < mas.length; k++) {
+        for (let i = 0; i < bracketsConfig.length; i++) {
+            if (mas[k] == bracketsConfig[i][1] && stack[stack.length - 1] == bracketsConfig[i][0]) {
+                stack.pop();
+            } else if (mas[k] == bracketsConfig[i][0]) {
+                stack.push(mas[k]);
+            }
+        }
+    }
+    if (stack.length != 0) solution = false;
+    return solution;
 }
+
